63a6d8385e6e044d4642c581011062a544808c37
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2024-09-14 08:21:41 +1000

    Update search3.py

--- search3.py
+++ search3.py
@@ -2,20 +2,15 @@ from youtubesearchpython import VideosSearch
 
 videosSearch = VideosSearch('Highlights NFL 2024', limit = 20)
 
-print('<h1>NFL Highlights</h1>', file=open('/var/www/html/nfl.html', 'w'))
+# print('<h1>NFL Highlights</h1>', file=open('/var/www/html/nfl.html', 'w'))
 
 for i in range(10):
-    # print(videosSearch.result()['result'][i]['link'])
-
-    # title = json['entry']['title']['$t']
-    # author = json['entry']['author'][0]['name']
-    # print ("id:%s\nauthor:%s\ntitle:%s" % (id, author, title))
-
     title = videosSearch.result()['result'][i]['title']
     link = videosSearch.result()['result'][i]['link']
     posted = videosSearch.result()['result'][i]['publishedTime']
-    print ( "%s %s ( %s )" % (title,link,posted) )
+    # print ( "%s %s ( %s )" % (title,link,posted) )
+    print ( "<a href='%s'> %s ( %s ) </a><br>" % (link,title,posted) )
 
-    with open('/var/www/html/nfl.html', 'a') as f:
-      print ( "<a href='%s'> %s ( %s ) </a><br>" % (link,title,posted), file=f )
+    #with open('/var/www/html/nfl.html', 'a') as f:
+      #print ( "<a href='%s'> %s ( %s ) </a><br>" % (link,title,posted), file=f )
       # print('Filename:', filename, file=f)  # Python 3.x
